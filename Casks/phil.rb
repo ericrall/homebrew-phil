b483291f5360b67a723eb6c454c2b89641c43a68
Eric Rall <rall.eric@gmail.com>
2026-06-13 01:18:50 -0400

    phil 1.0.9

--- Casks/phil.rb
+++ Casks/phil.rb
@@ -1,6 +1,6 @@
 cask "phil" do
-  version "1.0.8"
-  sha256 "a8f07bc69bafdfbfd58ab62b03a90bd3d9a84736387873f6e3ad255b0e899b25"
+  version "1.0.9"
+  sha256 "06fcf14c1f794edf9ee17f71f2df10121eada2c9deef74cfe799833b56af8e29"
 
   url "https://rall.digital/phil/phil-#{version}.zip"
   name "Phil"
